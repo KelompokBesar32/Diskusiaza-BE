6d9174a8b385953449d3bc41e00343a64e74c9b9
aryafisil <aryafisi@gmail.com>
2022-07-13 12:55:10 +0700

    Penambahan fitur Create, Edit, Show, Delete Ruang

--- app/http/controllers/ruang/ruang.go
+++ app/http/controllers/ruang/ruang.go
@@ -1,55 +1,89 @@
 package ruang
 
 import (
+	"Diskusiaza-BE/app/http/middleware"
 	"Diskusiaza-BE/app/model"
+	"Diskusiaza-BE/constants"
 	"Diskusiaza-BE/database"
 	"net/http"
 
-	"github.com/go-playground/validator"
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
-func createRuang(c echo.Context) error {
-	validate := validator.New()
-	ruang := model.Ruang{}
-	_ = c.Bind(&ruang)
-
-	if err := validate.Struct(ruang); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"message": "ada inputan yang tidak sesuai",
-		})
+func createRuangController(c echo.Context) error {
+	judul := c.FormValue("judul")
+	deskripsi := c.FormValue("deskripsi")
+	userID := c.Get("user_id").(int)
+	ruang := model.Ruang{
+		Judul:     judul,
+		Deskripsi: deskripsi,
+		UserID:    userID,
 	}
 
-	database.DB.Create(&ruang)
+	tokenHeader := c.Request().Header.Get("Authorization")
+	token := tokenHeader[len(constants.TokenJwtType):]
+	user := middleware.GetDataFromToken(token)
+	userID = int(user["id"].(float64))
 
+	err := database.DB.Create(&ruang).Error
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "proses create ruang gagal",
+		})
+	}
+	database.DB.Create(&ruang)
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"message": "proses create ruang berhasil",
+		"ruang":   ruang,
 	})
+
 }
 
-func getRuang(c echo.Context) error {
+func getRuangController(c echo.Context) error {
 	ruang := model.Ruang{}
 	database.DB.Find(&ruang)
-
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"ruang": ruang,
+		"message": "proses get ruang berhasil",
+		"ruang":   ruang,
 	})
 }
 
-func putRuang(c echo.Context) error {
-	validate := validator.New()
+func EditRuangController(c echo.Context) error {
+
+	judul := c.FormValue("judul")
+	deskripsi := c.FormValue("deskripsi")
+	userID := c.Get("user_id").(int)
+	ruang := model.Ruang{
+		Judul:     judul,
+		Deskripsi: deskripsi,
+		UserID:    userID,
+	}
+
+	tokenHeader := c.Request().Header.Get("Authorization")
+	token := tokenHeader[len(constants.TokenJwtType):]
+	user := middleware.GetDataFromToken(token)
+	userID = int(user["id"].(float64))
+
+	err := database.DB.Save(&ruang).Error
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "proses edit ruang gagal",
+		})
+	}
+	database.DB.Save(&ruang)
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "proses edit ruang berhasil",
+		"ruang":   ruang,
+	})
+}
+
+func deleteRuangController(c echo.Context) error {
 	ruang := model.Ruang{}
 	_ = c.Bind(&ruang)
 
-	if err := validate.Struct(ruang); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"message": "ada inputan yang tidak sesuai",
-		})
-	}
-
-	database.DB.Save(&ruang)
+	database.DB.Delete(&ruang)
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "proses update ruang berhasil",
+		"message": "proses delete ruang berhasil",
 	})
 }
